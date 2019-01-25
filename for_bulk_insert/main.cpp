f82ba7d94747452f0cda78016aaa6f24c64a427c
Atul Sharma <atulsharma406@gmail.com>
2019-01-25 15:24:54 +0530

    Revert "Updated bulk_insert application. Now works with free function"
    This reverts commit 77c58ce3e77b9add475f8b23bce5c08b15ccafbf.

--- for_bulk_insert/main.cpp
+++ for_bulk_insert/main.cpp
@@ -1,7 +1,5 @@
 #include "security_list_struct.hpp"
 
-#include "db_log_info.hpp"
-
 #include <iostream>
 #include <string>
 #include <vector>
@@ -18,7 +16,7 @@ int main()
     std::cout << obj2 << "\n";
 #endif 
 
-    auto obj3 = security_list{};
+    auto obj3 = security_list{color};
     
     auto const result_rows = obj3.read_file("somefile");
     
@@ -30,17 +28,16 @@ int main()
 #endif
 
 /*
-                   std::vector<security_list>  const & result
+				   std::vector<security_list>  const & result
                   ,std::string                 const & gtt_table
                   ,std::size_t                 const & batch_size
                   ,std::string                 const & price_date
                   ,std::string                 const & invocation_id
                   ,std::string                 const & run_id
                   ,bool                                was_loaded)
-*/  
+*/	
     
-    auto const & log_info_object = db_log_info{"price_date_val", "invocation_id_value", "run_id_value"};
-    obj3.load_data(/*db,*/ result_rows, "gtt_table_val", 20000, log_info_object, true);
-    obj3.execute(/*db,*/ "gtt_table_val");
+	obj3.load_data(/*db,*/ result_rows, "gtt_table_val", 20000, "price_date_val", "invocation_id_val", "run_id_val", true);
+	obj3.execute(/*db,*/ "gtt_table_val");
     
 }
