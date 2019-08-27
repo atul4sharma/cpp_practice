8fa1c680ee3e8dbedddb0970eb03db7ad8234840
Atul Sharma <atulsharma406@gmail.com>
2019-08-27 19:36:43 +0530

    Update printing function in .main.cpp

--- .main.cpp
+++ .main.cpp
@@ -1,4 +1,5 @@
 
+#include <map>
 #include <vector>
 #include <string>
 #include <cctype>
@@ -6,6 +7,25 @@
 #include <iterator>
 #include <iostream>
 #include <algorithm>
+
+template <typename T, typename U>
+auto operator << (std::ostream & out, std::pair<T, U> const & item)
+    -> std::ostream &
+{
+    out << "(" << item.first << ", " << item.second << ")";
+    return out;
+}
+
+template <typename T, typename U>
+auto operator << (std::ostream & out, std::map<T, U> const & v)
+    -> std::ostream &
+{
+    std::cout << "{";
+    for(auto const & item : v)
+        std::cout << item << ", ";
+    std::cout << "}";
+    return out;
+}
 
 template <typename T>
 auto operator << (std::ostream & out, std::vector<T> const & v)
