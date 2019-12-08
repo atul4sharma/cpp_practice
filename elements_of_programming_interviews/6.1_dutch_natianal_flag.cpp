73240fdd2917e93ab1206fd28d4d1ebb5d94eba4
Atul Sharma <atulsharma406@gmail.com>
2019-12-08 18:03:18 +0530

    Update dutch flag example

--- elements_of_programming_interviews/6.1_dutch_natianal_flag.cpp
+++ elements_of_programming_interviews/6.1_dutch_natianal_flag.cpp
@@ -8,39 +8,51 @@
 #include <iostream>
 #include <algorithm>
 
+enum class color { red, white, blue };
+
+auto to_string(color const item)
+    -> std::string
+{
+    switch(item)
+    {
+        case color::red   : return "RED";
+        case color::white : return "WHITE";
+        case color::blue  : return "BLUE";
+    }
+    return std::string{};
+}
+
 inline auto random_vector()
 {
-    auto v = std::vector<int>(10);
-    std::iota(v.begin(), v.end(), 1);
+    auto v = std::vector<color>{};
     std::srand(std::time(0));
-    auto random_index = std::rand() % static_cast<int>(v.size() - 1);
-    v.insert(v.begin() + random_index, 3, *(v.begin() + random_index));
-    random_index = std::rand() % static_cast<int>(v.size() - 1);
-    v.insert(v.begin() + random_index, 2, *(v.begin() + random_index));
+    auto random_index = std::rand() % static_cast<int>(10) + 1;
+    v.insert(v.begin(), random_index, color::red);
+    random_index = std::rand() % static_cast<int>(5) + 1;
+    v.insert(v.begin(), random_index, color::white);
+    random_index = std::rand() % static_cast<int>(5) + 1;
+    v.insert(v.begin(), random_index, color::blue);
 
     std::random_shuffle(v.begin(), v.end());
     return v;
 }
 
 template <typename Iterator>
-auto quicksort(Iterator first, Iterator last)
+auto partition_color(Iterator first, Iterator last)
 {
-    if( first == last or std::next(first) == last )
-        return;
-
-    auto const pivot = *first;
-    auto partition_point = std::partition(std::next(first), last, [&pivot](auto const item) { return item < pivot;});
-    std::iter_swap(first, std::prev(partition_point));
-    quicksort(first, std::prev(partition_point));
-    quicksort(partition_point, last);
+    std::partition(std::partition(first
+                                 ,last
+                                 ,[](auto const item) { return item < color::white;})
+                  ,last
+                  ,[](auto const item) { return item == color::white;});
 }
 
 int main()
 {
     auto v = random_vector();
 
-    println(std::cout << "Before sort => ", v);
-    quicksort(v.begin(), v.end());
-    println(std::cout << "After sort  => ", v);
+    println(std::cout << "Before parition => ", v);
+    partition_color(v.begin(), v.end());
+    println(std::cout << "After parition  => ", v);
 
 }
